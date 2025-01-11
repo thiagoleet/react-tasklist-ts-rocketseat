047ef6bb27c2dae9d45e62c5b3b991b8ea6c8fed
Thiago Leite <thiagofmleite@gmail.com>
2025-01-11 11:32:17 -0300

    Implement task management functionality: add task input handling, disable button when input is empty, and display task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
+import React from "react";
 import styles from "./App.module.css";
+import { Task } from "./components/Task";
 
 export default function App() {
+  const [tasks, setTasks] = React.useState<string[]>([]);
+  const [newTask, setNewTask] = React.useState<string>("");
+
+  const isNewTaskFieldClear = newTask.trim() === "";
+
+  function handleFormSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleTaskInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
   return (
     <main className={styles.container}>
       <header>
         <h1>Task List</h1>
       </header>
       <div className={styles.formContainer}>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <input
             type="text"
             placeholder="Task"
+            onChange={handleTaskInputChange}
           />
-          <button type="submit">Add Task</button>
+          <button
+            type="submit"
+            disabled={isNewTaskFieldClear}
+          >
+            Add Task
+          </button>
         </form>
       </div>
-      <footer></footer>
+      <div className={styles.taskList}>
+        <ul>
+          {tasks.map((task, index) => (
+            <Task key={index} />
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
