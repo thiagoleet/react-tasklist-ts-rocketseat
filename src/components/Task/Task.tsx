6b32cf17f115a0a187c8bdd010d9c7b02c3aff80
Thiago Leite <thiagofmleite@gmail.com>
2025-01-11 11:48:26 -0300

    Add TaskItem model and update Task component: implement task display with formatted date and description

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,9 +1,24 @@
+import { useFormatDate } from "../../hooks/useFormatDate";
+import { TaskItem } from "../../models/TaskItem";
 import styles from "./Task.module.css";
 
-export const Task = () => {
+interface TaskProps {
+  task: TaskItem;
+}
+
+export const Task = ({ task }: TaskProps) => {
+  const [publishedDateFormated, publishedDateRelativeToNow, ISODate] =
+    useFormatDate(task.createdAt);
+
   return (
-    <div className={styles.task}>
-      <h2>Task</h2>
-    </div>
+    <li className={styles.task}>
+      <p>{task.description}</p>
+      <time
+        title={publishedDateFormated}
+        dateTime={ISODate}
+      >
+        Criada {publishedDateRelativeToNow}
+      </time>
+    </li>
   );
 };
---
+++ src/models/TaskItem.ts
@@ -0,0 +1,5 @@
+export interface TaskItem {
+  id: string;
+  description: string;
+  createdAt: Date;
+}
