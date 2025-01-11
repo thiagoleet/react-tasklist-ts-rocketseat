3df413ea374bf106ddd2cb35b4368a4a81ad572e
Thiago Leite <thiagofmleite@gmail.com>
2025-01-11 11:20:58 -0300

    Add App module styles and implement Task List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import styles from "./App.module.css";
+
 export default function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <main className={styles.container}>
+      <header>
+        <h1>Task List</h1>
+      </header>
+      <div className={styles.formContainer}>
+        <form>
+          <input
+            type="text"
+            placeholder="Task"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+      <footer></footer>
+    </main>
+  );
 }
---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,9 @@
+import styles from "./Task.module.css";
+
+export const Task = () => {
+  return (
+    <div className={styles.task}>
+      <h2>Task</h2>
+    </div>
+  );
+};
